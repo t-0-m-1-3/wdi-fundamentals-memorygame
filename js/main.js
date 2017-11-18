00113f4792754ec96139e748510170cc30cbbcac
t0m <20537914+t-0-m-1-3@users.noreply.github.com>
2017-11-18 00:15:04 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running!");
+
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardThree);
