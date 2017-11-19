b77cc6f3aeb3f42f52bbd320f38138a43b805405
t0m <20537914+t-0-m-1-3@users.noreply.github.com>
2017-11-19 18:55:59 -0500

    pushing third draft

--- js/main.js
+++ js/main.js
@@ -32,11 +32,12 @@ var checkForMatch = function() {
 }
 
 
-var flipCard = function(cardId) {
+var flipCard = function() {
 // body of function
-	console.log("User flipped " + this);
-	cardsInPlay.push(this);
-	console.log("User flipped " + this.cardImage);
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + this.[1]);
+	cardsInPlay.push(this.rank);
+	console.log("User flipped " + this.[0]);
 	console.log("User flipped " + this.suit);
 	
 	checkForMatch();
@@ -52,7 +53,7 @@ var createBoard = function(){
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
 		cardElement.addEventListener('click', flipCard());
-		document.getElementById('game-board').appendChild(this.rank);
+		document.getElementById('game-board').appendChild(cardId);
 	
 	}
 }
